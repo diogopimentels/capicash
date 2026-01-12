a8bcf8516b04f9d461053f65ce274c1b5a8338e8
Diogo Pimentel <pimentel.bm01@gmail.com>
2026-01-12 17:37:35 -0300

    Reset Capicash 2.0 - O Inicio

--- backend/Capicash.API/Settings/AsaasSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Capicash.API.Settings;
-
-public class AsaasSettings
-{
-    public string ApiKey { get; set; } = string.Empty;
-    public string ApiUrl { get; set; } = string.Empty;
-    public string WebhookAccessToken { get; set; } = string.Empty;
-    public string MasterWalletId { get; set; } = string.Empty;
-}
